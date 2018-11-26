306d2d5d294c8abd97563fe25f21d11eb64ce53f
Ethoe <ryanouttrim@gmail.com>
2018-11-26 14:46:35 -0700

    base code added for sorts

--- sorts.cpp
+++ sorts.cpp
@@ -2,5 +2,36 @@
 
 sorts::sorts()
 {
+}
 
+std::vector<int> sorts::sorter(int sort, std::vector<int> list, int iterationStep)
+{
+    switch(sort)
+    {
+    case 1: return selection(list, iterationStep);
+    case 2: return insertion(list, iterationStep);
+    case 3: return bubble(list, iterationStep);
+    case 4: return merge(list, iterationStep);
+    }
+    return list;
+}
+
+std::vector<int> sorts::selection(std::vector<int> list, int iterationStep)
+{
+    return list;
+}
+
+std::vector<int> sorts::insertion(std::vector<int> list, int iterationStep)
+{
+    return list;
+}
+
+std::vector<int> sorts::bubble(std::vector<int> list, int iterationStep)
+{
+    return list;
+}
+
+std::vector<int> sorts::merge(std::vector<int> list, int iterationStep)
+{
+    return list;
 }
--- sorts.h
+++ sorts.h
@@ -1,11 +1,20 @@
 #ifndef SORTS_H
 #define SORTS_H
 
+#include <vector>
 
 class sorts
 {
+
 public:
     sorts();
+    std::vector<int> sorter(int sort, std::vector<int> list, int iterationStep);
+
+private:
+    std::vector<int> insertion(std::vector<int> list, int iterationStep);
+    std::vector<int> selection(std::vector<int> list, int iterationStep);
+    std::vector<int> merge(std::vector<int> list, int iterationStep);
+    std::vector<int> bubble(std::vector<int> list, int iterationStep);
 };
 
 #endif // SORTS_H
